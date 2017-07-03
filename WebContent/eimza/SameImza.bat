094a389395664e6242d237b83510ae1308fe27d4
Dsinan <sinandobur@gmail.com>
2017-07-03 21:31:15 +0300

    setting

---
+++ WebContent/eimza/SameImza.bat
@@ -0,0 +1,19 @@
+cd "c:\Program Files\Java\jdk1.8.0_131\bin"
+c:
+jarsigner.exe -keystore D:\WORKSPACE\mza\myKeyStore D:\WORKSPACE\mzaweb\WebContent\eimza.jar me
+#jarsigner.exe -keystore D:\WORKSPACE\mza\myKeyStore D:\WORKSPACE\mzaweb\WebContent\eimza\akiscif-2.5.4.jar me
+#jarsigner.exe -keystore D:\WORKSPACE\mza\myKeyStore D:\WORKSPACE\mzaweb\WebContent\eimza\asn1rt-6.6.4.jar me
+#jarsigner.exe -keystore D:\WORKSPACE\mza\myKeyStore D:\WORKSPACE\mzaweb\WebContent\eimza\axis-1.4.jar me
+#jarsigner.exe -keystore D:\WORKSPACE\mza\myKeyStore D:\WORKSPACE\mzaweb\WebContent\eimza\ccid-1.1.5.jar me
+#jarsigner.exe -keystore D:\WORKSPACE\mza\myKeyStore D:\WORKSPACE\mzaweb\WebContent\eimza\commons-discovery-0.2.jar me
+#jarsigner.exe -keystore D:\WORKSPACE\mza\myKeyStore D:\WORKSPACE\mzaweb\WebContent\eimza\commons-logging-1.1.1.jar me
+#jarsigner.exe -keystore D:\WORKSPACE\mza\myKeyStore D:\WORKSPACE\mzaweb\WebContent\eimza\hamcrest-core-1.3.jar me
+#jarsigner.exe -keystore D:\WORKSPACE\mza\myKeyStore D:\WORKSPACE\mzaweb\WebContent\eimza\itextpdf-5.5.11.jar me
+#jarsigner.exe -keystore D:\WORKSPACE\mza\myKeyStore D:\WORKSPACE\mzaweb\WebContent\eimza\jaxrpc-api-1.1.jar me
+#jarsigner.exe -keystore D:\WORKSPACE\mza\myKeyStore D:\WORKSPACE\mzaweb\WebContent\eimza\jcifs-1.2.19.jar me
+#jarsigner.exe -keystore D:\WORKSPACE\mza\myKeyStore D:\WORKSPACE\mzaweb\WebContent\eimza\jna-4.2.1.jar me
+#jarsigner.exe -keystore D:\WORKSPACE\mza\myKeyStore D:\WORKSPACE\mzaweb\WebContent\eimza\junit-4.11.jar me
+#jarsigner.exe -keystore D:\WORKSPACE\mza\myKeyStore D:\WORKSPACE\mzaweb\WebContent\eimza\slf4j-api-1.6.6.jar me
+#jarsigner.exe -keystore D:\WORKSPACE\mza\myKeyStore D:\WORKSPACE\mzaweb\WebContent\eimza\slf4j-jdk14-1.6.6.jar me
+#jarsigner.exe -keystore D:\WORKSPACE\mza\myKeyStore D:\WORKSPACE\mzaweb\WebContent\eimza\sqlite-jdbc-3.7.2.jar me
+#jarsigner.exe -keystore D:\WORKSPACE\mza\myKeyStore D:\WORKSPACE\mzaweb\WebContent\eimza\wsdl4j-1.6.2.jar me
